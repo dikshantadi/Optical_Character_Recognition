918a793c0d0b47046b67d66a95e0e2d0009b33f7
Dikshyant Adhikari <adhikarybad@gmail.com>
2025-04-02 19:45:47 +0545

    gaussian blue

--- main.py
+++ main.py
@@ -4,5 +4,13 @@ import matplotlib.pyplot as plt
 
 image = cv2.imread("resources/AI.jpg", cv2.IMREAD_COLOR_BGR)
 
-plt.imshow(image, cmap= "gray")
-plt.show()
+target_width = 600
+h, w = image.shape[:2]
+target_height = int((target_width / w) * h)
+
+image = cv2.resize(image, (target_width, target_height), interpolation=cv2.INTER_AREA) 
+image1 = cv2.GaussianBlur(image, (3, 3), 1)
+
+cv2.imshow("resize for now", image1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()

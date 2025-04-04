2584b20f9437095cad4c2b45287304150281792a
Dikshyant Adhikari <adhikarybad@gmail.com>
2025-04-04 10:19:25 +0545

    aa

--- prepro.py
+++ prepro.py
@@ -1,31 +1,30 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 
-image = cv2.imread("resources/sh.jpg", cv2.IMREAD_COLOR)
-
-# to resize the image
+#resize
+image = cv2.imread("resources/sh.jpg")
 target_width = 700
 h, w = image.shape[:2]
 target_height = int((target_width / w) * h)
 image = cv2.resize(image, (target_width, target_height), interpolation=cv2.INTER_AREA)
 
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-blurred_image = cv2.GaussianBlur(gray_image, (3, 3), 0) #reduces noise
-#blurred_image = cv2.fastNlMeansDenoising(blurred_image, h=10)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-#adaptive thresholding
-img_adaptive_mean = cv2.adaptiveThreshold(
-    blurred_image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 5
+gray = cv2.GaussianBlur(gray, (3, 3), 0)
+
+# Sharpen before thresholding (so edges are stronger in binary image)
+sharpen_kernel = np.array([[0, -1, 0],
+                           [-1, 5, -1],
+                           [0, -1, 0]])
+sharpened = cv2.filter2D(gray, -1, sharpen_kernel)
+
+# Adaptive Thresholding
+adaptive = cv2.adaptiveThreshold(
+    sharpened, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2
 )
-img_edge = cv2.Canny(img_adaptive_mean, 100, 200)
 
-kernel = np.array([[ 0, -1,  0],
-                   [-1,  5, -1],
-                   [ 0, -1,  0]])
-
-img_sharp = cv2.filter2D(img_edge, -1, kernel)
-cv2.imshow("Adaptive Thresholding", img_sharp)
+# Show output
+cv2.imshow("OCR Prep Image", adaptive)
 cv2.waitKey(0)
 cv2.destroyAllWindows()

1955b09697547587c0b6ae520aee11cf192b0ad0
Dikshyant Adhikari <adhikarybad@gmail.com>
2025-04-04 10:25:05 +0545

    aa

--- prepro.py
+++ prepro.py
@@ -8,7 +8,6 @@ h, w = image.shape[:2]
 target_height = int((target_width / w) * h)
 image = cv2.resize(image, (target_width, target_height), interpolation=cv2.INTER_AREA)
 
-
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 gray = cv2.GaussianBlur(gray, (3, 3), 0)
@@ -24,7 +23,6 @@ adaptive = cv2.adaptiveThreshold(
     sharpened, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2
 )
 
-# Show output
 cv2.imshow("OCR Prep Image", adaptive)
 cv2.waitKey(0)
 cv2.destroyAllWindows()

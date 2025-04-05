5007c184f60074b79b1dfd0b819402debcb0d57d
Dikshyant Adhikari <adhikarybad@gmail.com>
2025-04-05 18:23:29 +0545

    ocr

--- ocr.py
+++ ocr.py
@@ -3,6 +3,7 @@ import cv2
 import numpy as np 
 
 preprocessed_img = cv2.imread("preprocessed.jpg", cv2.IMREAD_COLOR_BGR)
+#preprocessed_img = cv2.imread("resources/dsp.jpg", cv2.IMREAD_COLOR_BGR)
 
-text = pytesseract.image_to_string(preprocessed_img)
+text = pytesseract.image_to_string(preprocessed_img, config='--psm 6')
 print(text)
--- prepro.py
+++ prepro.py
@@ -3,7 +3,7 @@ import numpy as np
 
 #resize
 image = cv2.imread("resources/dsp.jpg")
-target_width = 700
+target_width = 1000
 h, w = image.shape[:2]
 target_height = int((target_width / w) * h)
 image = cv2.resize(image, (target_width, target_height), interpolation=cv2.INTER_AREA)
@@ -14,7 +14,7 @@ gray = cv2.GaussianBlur(gray, (3, 3), 0)
 
 # Sharpen before thresholding (so edges are stronger in binary image)
 sharpen_kernel = np.array([[0, -1, 0],
-                           [-1, 5, -1],
+                           [-1, 5.15, -1],
                            [0, -1, 0]])
 sharpened = cv2.filter2D(gray, -1, sharpen_kernel)
 

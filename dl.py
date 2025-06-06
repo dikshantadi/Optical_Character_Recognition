641a9a7cfea9b139977bcd7a2adc11d43a2cefb8
Dikshyant Adhikari <adhikarybad@gmail.com>
2025-06-06 14:16:31 +0545

    resource

--- dl.py
+++ dl.py
@@ -1,5 +1,31 @@
-import cv2
-import tensorflow as tf 
-import numpy as np 
-import matplotlib.pyplot as plt 
+import tensorflow as tf
+import numpy as np
+import kagglehub
 
+path = kagglehub.dataset_download("preatcher/standard-ocr-dataset")
+
+print("Path to dataset files:", path)
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.reshape(-1, 28, 28, 1).astype('float32') / 255.0
+x_test = x_test.reshape(-1, 28, 28, 1).astype('float32') / 255.0
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D((2,2)),
+
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2,2)),
+
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+
+model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=3)
+
+model.save('cnn_ocr.5')
--- ocr.py
+++ ocr.py
@@ -1,11 +1,44 @@
 import pytesseract
 import cv2
 import numpy as np 
+import tensorflow as tf
 
-def perform_ocr(image_path, output_txt="output.txt"):
-    img = cv2.imread(image_path)
-    text = pytesseract.image_to_string(img, config='--psm 6')
+model = tf.keras.models.load_model("cnn_ocr.h5")
+
+def preprocess_digit(cropped_img):
+    resized = cv2.resize(cropped_img, (28, 28))
+    normalized = resized / 255.0
+    return normalized.reshape(1, 28, 28, 1)
+
+def perform_ocr(image_path="preprocessed.jpg", output_txt="output.txt"):
+    img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+    _, img = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY)
+
+    contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    digits = []
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        if w > 5 and h > 10:
+            digit_img = img[y:y+h, x:x+w]
+            input_digit = preprocess_digit(digit_img)
+            prediction = model.predict(input_digit, verbose=0)
+            digit = np.argmax(prediction)
+            digits.append((x, y, digit))
+
+    # Sort by y (rows), then x (left-to-right)
+    digits.sort(key=lambda tup: (tup[1]//50, tup[0]))  # group by ~50px vertical spacing
+
+    text = ""
+    current_line = -1
+    for x, y, digit in digits:
+        line = y // 50
+        if line != current_line:
+            text += "\n"  # new row
+            current_line = line
+        text += str(digit) + " "
+
     with open(output_txt, "w", encoding="utf-8") as f:
         f.write(text)
-    return text
 
+    return text
